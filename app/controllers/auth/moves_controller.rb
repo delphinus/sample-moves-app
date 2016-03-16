4b24d70d9ccf6f9a21a5a3556dc6c39d9c68d1c9
delphinus <delphinus@remora.cx>
2016-03-16 23:43:50 +0900

    Add /auth/moves/callback

---
+++ app/controllers/auth/moves_controller.rb
@@ -0,0 +1,17 @@
+class Auth::MovesController < ApplicationController
+
+  include MovesHelper
+
+  def callback
+
+    new_token = Moves.client.auth_code.get_token(callback_params[:code], redirect_uri: moves_redirect_uri)
+    session[:access_token] = new_token.token
+    redirect_to root_path
+  end
+
+  private
+
+    def callback_params
+      params.permit :code
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
 
+  namespace :auth do
+
+    resources :moves, only: [] do
+
+      collection do
+        get :callback
+      end
+    end
+  end
+
   root to: 'root#index', via: [:get]
 end
