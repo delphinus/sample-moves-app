392bf86606ab00e20b0994e40cdaf6ab09f19b5e
delphinus <delphinus@remora.cx>
2016-03-16 23:35:54 +0900

    Add logic for root#index

--- app/controllers/root_controller.rb
+++ app/controllers/root_controller.rb
@@ -2,5 +2,12 @@ class RootController < ApplicationController
 
   def index
 
+    if session[:access_token].blank?
+      @moves_authorize_uri = Moves.client.auth_code.authorize_url(
+        redirect_uri: callback_auth_moves_url,
+        scope: 'activity'
+      )
+      render 'root/signin'
+    end
   end
 end
